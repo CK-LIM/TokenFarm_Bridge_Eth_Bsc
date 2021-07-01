51bd143062ab58d23ba104d58b083dd00c95af92
Ubuntu <cheekeat_lim1991@hotmail.com>
2021-07-01 20:24:19 +0800

    Updates

---
+++ precache-manifest.c75c3902a0dd647e3a907e07e2f1f523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b5cdc06931e6747407c",
+    "url": "/TokenFarm_Bridge_Eth_Bsc/static/css/main.bf77cb42.chunk.css"
+  },
+  {
+    "revision": "4b5cdc06931e6747407c",
+    "url": "/TokenFarm_Bridge_Eth_Bsc/static/js/main.4b5cdc06.chunk.js"
+  },
+  {
+    "revision": "24c7b64152fcdf3ba4a6",
+    "url": "/TokenFarm_Bridge_Eth_Bsc/static/css/1.efb3ddd9.chunk.css"
+  },
+  {
+    "revision": "24c7b64152fcdf3ba4a6",
+    "url": "/TokenFarm_Bridge_Eth_Bsc/static/js/1.24c7b641.chunk.js"
+  },
+  {
+    "revision": "d44a7d0111da7594d3d2",
+    "url": "/TokenFarm_Bridge_Eth_Bsc/static/js/runtime~main.d44a7d01.js"
+  },
+  {
+    "revision": "faf6855446a174fb8ca7a6c6d87fde12",
+    "url": "/TokenFarm_Bridge_Eth_Bsc/index.html"
+  }
+];
